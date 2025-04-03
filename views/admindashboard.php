5fd4b7de8e575177819c03222c51334a3e0a32c2
segaak <satriowicak878@gmail.com>
2025-04-04 01:31:54 +0700

    last kaya.x

---
+++ views/admindashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <section id="sidebar">
+        <a href="#" class="brand">
+            <img src=".../images/logo.png" alt="Logo">
+        </a>
+        
+
+        <ul class="side-menu-top">
+            <li>
+                <a href="#">
+                <i class='bx bxs-dashboard'></i>
+                <span class="text">Dashboard</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+</body>
+</html>
