fecd7eb96b46c8e5a86219c8f879f96843043c34
GabrielEgi <124230096@student.upnyk.ac.id>
2025-06-16 15:12:17 +0700

    AAA

--- views/customer/produk.php
+++ views/customer/produk.php
@@ -141,7 +141,6 @@ session_start();
       btn.classList.add('active');
 
       const filter = btn.getAttribute('data-filter');
-
       items.forEach(item => {
         if (filter === 'all') {
           item.style.display = 'block';
